73fd57c7cee4d7c16b0746bac76d958a24342135
Hassaan Sami <hassaan.sami@live.com>
2019-08-16 18:43:35 -0400

    Add table to tooltip

--- app.js
+++ app.js
@@ -1,22 +1,99 @@
-$(document).ready(function() {
-    $('[data-toggle="tooltip"]').tooltip();
-    // $('[data-toggle="tooltip"]').tooltip('show'); 
+$(document).ready(function () {
+    $('.brand').attr('data-toggle', 'tooltip');
+    $('.brand').attr('data-html', 'true');
+    $('.brand').attr('data-template', tooltipTemplate);
+    $('.brand').attr('title', tooltipTitle2);
+
+    $('.brand').tooltip();
+    // $('.special').tooltip('show');
+    // insertTableInTooltip();
 
     $('.brand')
-    .mouseleave(event, function() {
-        if (event.toElement.className.includes("tooltip")) {
-            $(this).tooltip('show');
-            $('.tooltip').mouseleave(event, function() {
-                $('.brand').tooltip('hide');
-            });
-        }
-    });
+        // .mouseenter(event, function() {
+        //     insertTableInTooltip();
+        // })
+        .mouseleave(event, function () {
+            if (event.toElement.className.includes("tooltip") ||
+                event.toElement.className.includes("arrow")) {
+                $('.special').tooltip('show');
+                // insertTableInTooltip();
 
-    // $('.tooltip')
-    // .mouseout(event, function() {
-    //     console.log('fuck no');
-    //     $('.brand').tooltip('hide');
-    // });
-
-    // $('.brand').attr('title', '<em>tooooooool</em> <u>with</u> <b>HTML</b>');
+                $('.tooltip')
+                .mouseleave(event, function () {
+                    $('.brand').tooltip('hide');
+                });
+                
+            }
+        });
 });
+
+function insertTableInTooltip() {
+    $('.tooltip.show').find('.tooltip-inner').html(tooltipTitle2);
+}
+
+
+let tooltipTemplate = `
+    <div class="tooltip" role="tooltip">
+        <div class="arrow"></div>
+        <div class="tooltip-inner"></div>
+    </div>
+`;
+
+
+let tooltipTitle2 = `
+    <div class='tooltip-table-container'>
+        <div class='row table-titles'>
+            <div>
+                MARKET GROUP
+            </div>
+            <div>
+                SKU's
+            </div>
+        </div>
+        <div class='row'>
+            <div>
+                Baby
+            </div>
+            <div>
+                3
+            </div>
+        </div>
+        <div class='row'>
+            <div>
+                Electrical
+            </div>
+            <div>
+                2
+            </div>
+        </div>
+        <div class='moreBrands'>
+            + 15 more
+        </div>
+    </div>
+    <div class="btn-container">
+        <a class="open-btn" href="#">CLICK TO OPEN</a>
+    </div>
+`;
+
+let tooltipTitle = `
+    <table class="table table-borderless">
+        <thead>
+            <tr>
+                <th scope="col">MARKET GROUP</th>
+                <th scope="col">SKU's</th>
+            </tr>
+        </thead>
+        <tbody>
+            <tr>
+                <td>Baby</td>
+                <td>3</td>
+            </tr>
+            <tr>
+                <td>Electrical</td>
+                <td>2</td>
+            </tr>
+        </tbody>
+    </table>
+    <a class="open-btn" href="#">CLICK TO OPEN</a>
+`;
+
